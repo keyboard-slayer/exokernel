187591cbea654a155ca627a60f00f419358686e2
Keyboard Slayer <dalcjor@libmail.eu>
2022-07-15 19:59:31 +0200

    elf: fix

--- src/kernel/src/elf.c
+++ src/kernel/src/elf.c
@@ -29,15 +29,16 @@ task_t *loader_binary(void *elf)
         halt();
     }
 
-    Elf64_Phdr *program_header = (Elf64_Phdr *)((uint64_t)elf + header->e_phoff);
     void *space = vmm_create_space(); 
 
     for (size_t i = 0; i < header->e_phnum; i++)
     {
-        if (program_header[i].p_type == PT_LOAD)
+        Elf64_Phdr *program_header = (Elf64_Phdr *)((uintptr_t) elf + header->e_phoff + i * header->e_phentsize);
+
+        if (program_header->p_type == PT_LOAD)
         {
-            klog(INFO, "Loading Pheader %p -> %p", program_header[i].p_vaddr, program_header[i].p_vaddr + program_header[i].p_memsz);
-            void *addr = pmm_alloc(ALIGN_UP(program_header[i].p_memsz, PAGE_SIZE));
+            klog(INFO, "Loading Pheader %p -> %p", program_header->p_vaddr, program_header->p_vaddr + program_header->p_memsz);
+            void *addr = pmm_alloc(ALIGN_UP(program_header->p_memsz, PAGE_SIZE));
 
             if (addr == NULL)
             {
@@ -46,16 +47,14 @@ task_t *loader_binary(void *elf)
             }
 
             vmm_map(space, (virtual_physical_map_t) {
-                .virtual = ALIGN_DOWN(program_header[i].p_vaddr, PAGE_SIZE),
+                .virtual = ALIGN_DOWN(program_header->p_vaddr, PAGE_SIZE),
                 .physical = (uint64_t) addr,
-                .length = ALIGN_UP(program_header[i].p_memsz, PAGE_SIZE)
+                .length = ALIGN_UP(program_header->p_memsz, PAGE_SIZE)
             }, true);
 
-            __builtin_memcpy((void *)((uint64_t) addr + loader_get_hhdm()), (void *)((uint64_t)elf + program_header[i].p_offset), program_header[i].p_filesz);
-            __builtin_memcpy((void *)((uint64_t) addr + loader_get_hhdm() + program_header[i].p_filesz), (void *)((uint64_t)elf + program_header[i].p_offset + program_header[i].p_filesz), program_header[i].p_memsz - program_header[i].p_filesz);
+            __builtin_memcpy((void *)((uint64_t) addr + loader_get_hhdm()), (void *)((uint64_t)elf + program_header->p_offset), program_header->p_filesz);
+            __builtin_memcpy((void *)((uint64_t) addr + loader_get_hhdm() + program_header[i].p_filesz), (void *)((uint64_t)elf + program_header->p_offset + program_header->p_filesz), program_header->p_memsz - program_header->p_filesz);
         }
-
-        program_header = (Elf64_Phdr *)((uint64_t)program_header + (i * header->e_phentsize));
     }
 
     klog(INFO, "Entry point: %p", header->e_entry);
