c929a4919a0df9da00f1462a92d7b36db7eb8708
Keyboard Slayer <dalcjor@libmail.eu>
2022-07-17 17:50:01 +0200

    klibc: add vec

---
+++ src/klibc/src/vec.c
@@ -0,0 +1,26 @@
+#include <kernel/inc/logging.h>
+#include <kernel/inc/utils.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "../inc/vec.h"
+
+void vec_expand_(char **data, size_t *length, size_t *capacity, int memsz)
+{
+    if (*length + 1 > *capacity)
+    {
+        void *ptr;
+        size_t n = (*capacity == 0) ? 1 : *capacity << 1;
+        ptr = realloc(*data, n * memsz);
+
+        if (ptr == NULL)
+        {
+            klog(ERROR, "vec_expand: realloc failed");
+            halt();
+        }
+
+        *data = ptr;
+        *capacity = n;
+    }
+}
